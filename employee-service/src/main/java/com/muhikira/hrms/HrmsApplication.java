943ae69c9dea843ee97142c5971e4e27809b45ea
Muhikira <muhikiraismael@gmail.com>
2024-10-16 13:39:34 -0500

    After updating eemployee-service

--- employee-service/src/main/java/com/muhikira/hrms/HrmsApplication.java
+++ employee-service/src/main/java/com/muhikira/hrms/HrmsApplication.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
